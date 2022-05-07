f847076aa70f236ea5be20cdd6fc3da5e5696053
vietvuive579 <vietvuive579@gmail.com>
2022-05-07 18:14:03 +0700

    first commit

---
+++ api/serializers.py
@@ -0,0 +1,9 @@
+from dataclasses import field
+from pyexpat import model
+from rest_framework import serializers
+from base.models import Item
+
+class ItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Item
+        fields = '__all__'
