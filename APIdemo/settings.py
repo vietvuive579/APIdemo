61e59490d2c19526b9f7e8d370f41d2b1c076cf7
vietvuive579 <vietvuive579@gmail.com>
2022-05-11 09:19:04 +0700

    connect PostgreSQL

--- APIdemo/settings.py
+++ APIdemo/settings.py
@@ -78,9 +78,13 @@ WSGI_APPLICATION = 'APIdemo.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+      'ENGINE':'django.db.backends.postgresql_psycopg2',
+      'NAME':'APIdemo',
+      'USER':'postgres',
+      'PASSWORD':'NHViet579',
+      'HOST':'localhost',
+      'PORT':'5432',
+   }
 }
 
 
