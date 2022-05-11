70ae81fb57a509acc00b8764b115770ccaabd788
vietvuive579 <vietvuive579@gmail.com>
2022-05-11 11:55:23 +0700

    update base models

--- base/models.py
+++ base/models.py
@@ -1,8 +1,13 @@
 from statistics import mode
+from turtle import update
+import uuid
 from venv import create
 from django.db import models
 
 # Create your models here.
 class Item(models.Model):
+    uuid = models.UUIDField(default=uuid.uuid4, null=False)
     name = models.CharField(max_length=200)
-    created = models.DateTimeField(auto_now_add=True)
+    desciption = models.CharField(max_length=255, null=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    update_date = models.DateTimeField(auto_now=True)
